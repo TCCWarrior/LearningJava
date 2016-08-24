6853c0583ca88db7472792b76840a34d35836da6
TCCWarrior <astahlbusch@gmail.com>
2016-08-24 09:10:34 -0400

    Initial commit

---
+++ learnjava/src/main/java/com/example/Scratchpad.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Scratchpad {
+    public static void main(String[] args) {
+
+        String vehicle = "Truck";
+        String make = "Chevy";
+        String model = "Tahoe";
+
+        String fullVehicleDetails = vehicle + " - " + make + " : " + model;
+
+        System.out.println(fullVehicleDetails);
+
+        
+    }
+}
